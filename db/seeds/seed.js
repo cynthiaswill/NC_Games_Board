2679f0a8dd3286a7814cbb57189e80bf36085372
cynthiaswill <yzhang4@msn.com>
2021-11-01 19:57:10 +0000

    refactored seed with async await

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,41 +1,26 @@
 const db = require('../connection.js');
 const format = require('pg-format');
 
-const seed = (data) => {
+const seed = async (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
-  // 1. create tables
-  return db
-    .query(`DROP TABLE IF EXISTS comments;`)
-    .then(()=> {
-      return db
-      .query(`DROP TABLE IF EXISTS reviews;`)
-    })
-    .then(() => {
-      return db
-      .query(`DROP TABLE IF EXISTS users;`)
-    })
-    .then(() => {
-      return db
-      .query(`DROP TABLE IF EXISTS categories;`)
-    })
-    .then(() => {
-      return db
-        .query(`CREATE TABLE categories (
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS reviews;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+  await db.query(`DROP TABLE IF EXISTS categories;`);
+   // 1. create tables
+  await db
+    .query(`CREATE TABLE categories (
           slug VARCHAR PRIMARY KEY,
           description TEXT
         );`)
-    })
-    .then(() => {
-      return db
-        .query(`CREATE TABLE users (
+  await db
+    .query(`CREATE TABLE users (
           username VARCHAR PRIMARY KEY UNIQUE,
           avatar_url TEXT,
           name VARCHAR NOT NULL
         );`)
-    })
-    .then(() => {
-      return db
-        .query(`CREATE TABLE reviews (
+  await db
+    .query(`CREATE TABLE reviews (
           review_id SERIAL PRIMARY KEY,
           title VARCHAR NOT NULL,
           review_body TEXT NOT NULL,
@@ -49,10 +34,8 @@ const seed = (data) => {
           ON DELETE SET NULL,
           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         );`)
-    })
-    .then(() => {
-      return db
-        .query(`CREATE TABLE comments (
+  await db
+    .query(`CREATE TABLE comments (
           comment_id SERIAL PRIMARY KEY,
           author VARCHAR REFERENCES users(username)
           ON DELETE SET NULL,
@@ -62,30 +45,26 @@ const seed = (data) => {
           created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
           body TEXT NOT NULL
         )`)
-    })
     // 2. insert data
-    .then(() => {
-      const queryStr = format(
+      const queryStr1 = format(
         `INSERT INTO categories (slug, description)
           VALUES %L RETURNING *;`,
           categoryData.map((category) => {
             return [category.slug, category.description];
           })
       );
-      return db.query(queryStr);
-    })
-    .then(() => {
-      const queryStr = format(
+      await db.query(queryStr1);
+ 
+      const queryStr2 = format(
         `INSERT INTO users (username, name, avatar_url)
           VALUES %L RETURNING *;`,
           userData.map((user) => {
             return [user.username, user.name, user.avatar_url];
           })
       );
-      return db.query(queryStr);
-    })
-    .then(() => {
-      const queryStr = format(
+      await db.query(queryStr2);
+
+      const queryStr3 = format(
         `INSERT INTO reviews 
         (title, designer, owner, review_img_url, review_body, category, created_at, votes)
         VALUES %L RETURNING *;`,
@@ -93,10 +72,9 @@ const seed = (data) => {
           return [review.title, review.designer, review.owner, review.review_img_url, review.review_body, review.category, review.created_at, review.votes];
         })
       );
-      return db.query(queryStr);
-    })
-    .then(() => {
-      const queryStr = format(
+      await db.query(queryStr3);
+
+      const queryStr4 = format(
         `INSERT INTO comments
         (body, votes, author, review_id, created_at)
         VALUES %L RETURNING *;`,
@@ -104,8 +82,7 @@ const seed = (data) => {
         return [comment.body, comment.votes, comment.author, comment.review_id, comment.created_at];
         })
       );
-      return db.query(queryStr);
-    })
+      await db.query(queryStr4);
 };
 
 module.exports = seed;
