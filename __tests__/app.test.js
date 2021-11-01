2e82dea2666deee266f6684af5b2937779b02163
cynthiaswill <yzhang4@msn.com>
2021-11-01 23:31:28 +0000

    test get review by id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -59,7 +59,27 @@ describe('app', () => {
         })
     })
 
-
+    describe('/api/reviews/:review_id', () => {
+        test('status 200 returns get review by id correctly', () => {
+            return request(app)
+                .get('/api/reviews/2')
+                .expect(200)
+                .then(({ body }) => {
+                    expect(body.review).toEqual({
+                            review_id: 2,
+                            title: 'Jenga',
+                            review_body: 'Fiddly fun for all the family',
+                            designer: 'Leslie Scott',
+                            review_img_url: 'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+                            votes: 5,
+                            category: 'dexterity',
+                            owner: 'philippaclaire9',
+                            created_at: '2021-01-18T10:01:41.251Z'
+                    });
+                })
+        });
+        
+    })
     
 
 })
