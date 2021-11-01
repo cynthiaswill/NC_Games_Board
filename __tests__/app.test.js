af120c1f085b286b99f94cc2ae7abb0ec19bdc4c
cynthiaswill <yzhang4@msn.com>
2021-11-01 19:17:33 +0000

    added test for 404 bad path

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,23 @@
 const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
-const { seed } = require('../db/seeds/seed.js');
+const  seed  = require('../db/seeds/seed.js');
+const request = require('supertest');
+const app = require('../app')
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
+
+describe('app', () => {
+    test('test status 404 invalid url route', () => {
+        return request(app)
+            .get('/api/bad_route')
+            .expect(404)
+            .then(({ body }) => {
+                expect(body.msg).toBe('Invalid path');
+            });
+    });
+
+
+
+})
+    
