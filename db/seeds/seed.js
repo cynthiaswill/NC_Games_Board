c7d2f7c3fad1be7f4f96c0fd219a99264ecc7bc1
cynthiaswill <yzhang4@msn.com>
2021-11-01 15:16:27 +0000

    seed file done

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -2,6 +2,14 @@ const db = require('../connection.js');
 
 const seed = (data) => {
   const { categoryData, commentData, reviewData, userData } = data;
+  const createRef = (data, key, value) => {
+    const lookup = {};
+    for (let item of data) {
+        lookup[item[key]] = item[value];
+    }
+    return lookup;
+  };
+
   // 1. create tables
   return db
     .query(`DROP TABLE IF EXISTS categories;`)
@@ -15,59 +23,96 @@ const seed = (data) => {
     .then(() => {
       return db
         .query(`DROP TABLE IF EXISTS users;`)
-        .then(() => {
-          return db
-            .query(`CREATE TABLE users (
-              username VARCHAR PRIMARY KEY UNIQUE,
-              avatar_url TEXT,
-              name VARCHAR NOT NULL
-            );`)
-        })
-        .then(() => {
-          return db
-            .query(`DROP TABLE IF EXISTS reviews;`)
-        })
-        .then(() => {
-          return db
-            .query(`CREATE TABLE reviews (
-              review_id SERIAL PRIMARY KEY,
-              title VARCHAR NOT NULL,
-              review_body TEXT NOT NULL,
-              designer VARCHAR,
-              review_img_url TEXT 
-              DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
-              votes INT DEFAULT 0,
-              category VARCHAR REFERENCES categories(slug)
-              ON DELETE SET NULL,
-              owner VARCHAR REFERENCES users(username)
-              ON DELETE SET NULL,
-              created_at TIMESTAMP
-            );`)
-        })
-        .then(() => {
-          return db
-            .query(`DROP TABLE IF EXISTS comments;`)
-        })
-        .then(() => {
-          return db
-            .query(`CREATE TABLE comments (
-              comment_id SERIAL PRIMARY KEY,
-              author VARCHAR REFERENCES users(username)
-              ON DELETE SET NULL,
-              review_id INT REFERENCES reviews(review_id)
-              ON DELETE CASCADE,
-              votes INT NOT NULL,
-              created_at TIMESTAMP,
-              body TEXT NOT NULL
-            )`)
-        })
     })
-
-
-
-  // 2. insert data
-
-
+    .then(() => {
+      return db
+        .query(`CREATE TABLE users (
+          username VARCHAR PRIMARY KEY UNIQUE,
+          avatar_url TEXT,
+          name VARCHAR NOT NULL
+        );`)
+    })
+    .then(() => {
+      return db
+        .query(`DROP TABLE IF EXISTS reviews;`)
+    })
+    .then(() => {
+      return db
+        .query(`CREATE TABLE reviews (
+          review_id SERIAL PRIMARY KEY,
+          title VARCHAR NOT NULL,
+          review_body TEXT NOT NULL,
+          designer VARCHAR,
+          review_img_url TEXT 
+          DEFAULT 'https://images.pexels.com/photos/163064/play-stone-network-networked-interactive-163064.jpeg',
+          votes INT DEFAULT 0,
+          category VARCHAR REFERENCES categories(slug)
+          ON DELETE SET NULL,
+          owner VARCHAR REFERENCES users(username)
+          ON DELETE SET NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );`)
+    })
+    .then(() => {
+      return db
+        .query(`DROP TABLE IF EXISTS comments;`)
+    })
+    .then(() => {
+      return db
+        .query(`CREATE TABLE comments (
+          comment_id SERIAL PRIMARY KEY,
+          author VARCHAR REFERENCES users(username)
+          ON DELETE SET NULL,
+          review_id INT REFERENCES reviews(review_id)
+          ON DELETE CASCADE,
+          votes INT NOT NULL,
+          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          body TEXT NOT NULL
+        )`)
+    })
+    // 2. insert data
+    .then(() => {
+      const queryStr = format(
+        `INSERT INTO categories (slug, description)
+          VALUES %L RETURNING *;`,
+          categoryData.map((category) => {
+            return [category.slug, category.description];
+          })
+      );
+      return db.query(queryStr);
+    })
+    .then(() => {
+      const queryStr = format(
+        `INSERT INTO users (username, name, avatar_url)
+          VALUES %L RETURNING *;`,
+          userData.map((user) => {
+            return [user.username, user.name, user.avatar_url];
+          })
+      );
+      return db.query(queryStr);
+    })
+    .then(() => {
+      const queryStr = format(
+        `INSERT INTO reviews 
+        (title, designer, owner, review_img_url, review_body, category, created_at, votes)
+        VALUES %L RETURNING *;`,
+        reviewData.map((review) => {
+          return [review.title, review.designer, review.owner, review.review_img_url, review.review_body, review.category, review.created_at, review.votes];
+        })
+      );
+      return db.query(queryStr);
+    })
+    .then(() => {
+      const queryStr = format(
+        `INSERT INTO comments
+        (body, votes, author, review_id, created_at)
+        VALUES %L RETURNING *;`,
+        commentData.map((comment) => {
+        return [comment.body, comment.votes, comment.author, comment.review_id, comment.created_at];
+        })
+      );
+      return db.query(queryStr);
+    })
 };
 
 module.exports = seed;
