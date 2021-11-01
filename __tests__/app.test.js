e2a0b57057cbda32c52e5f2063a8c004a5a06085
cynthiaswill <yzhang4@msn.com>
2021-11-01 21:59:38 +0000

    status 200 /api/categories test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,8 @@ const db = require('../db/connection.js');
 const testData = require('../db/data/test-data/index.js');
 const  seed  = require('../db/seeds/seed.js');
 const request = require('supertest');
-const app = require('../app')
+const app = require('../app');
+const { string } = require('pg-format');
 
 beforeEach(() => seed(testData));
 afterAll(() => db.end());
@@ -17,6 +18,22 @@ describe('app', () => {
             });
     });
 
+    describe('/api/categories', () => {
+        test('status 200 returns all categories correctly', () => {
+            return request(app)
+                .get('/api/categories')
+                .expect(200)
+                .then(({ body }) => {
+                    expect(body.categories).toHaveLength(4);
+                    body.categories.forEach((category) => {
+                        expect(category).toMatchObject({
+                            slug: expect.any(String),
+                            description: expect.any(String)
+                        })
+                    })
+                })
+        })
+    })
 
 
 })
