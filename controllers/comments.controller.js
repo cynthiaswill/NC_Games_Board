e59449f4be0692738674f9fbbadc3fb764dc3e4e
cynthiaswill <yzhang4@msn.com>
2021-11-21 17:41:00 +0000

    added edit comment body into patchComment

--- controllers/comments.controller.js
+++ controllers/comments.controller.js
@@ -2,44 +2,48 @@ const {
     selectComments,
     insertComment,
     removeComment,
-    updateComment
-}   = require('../models/comments.model');
+    updateComment,
+} = require("../models/comments.model");
 
 exports.getComments = (req, res, next) => {
     const { review_id } = req.params;
     const { limit, p } = req.query;
     const queryKeys = Object.keys(req.query);
 
-    selectComments(review_id, limit, p, queryKeys).then(comments => {
-        res.send({ comments })
-    })
-    .catch(next)
-}
+    selectComments(review_id, limit, p, queryKeys)
+        .then((comments) => {
+            res.send({ comments });
+        })
+        .catch(next);
+};
 
 exports.postComment = (req, res, next) => {
     const { review_id } = req.params;
     const { username, body } = req.body;
 
-    insertComment(review_id, username, body).then(comment => {
-        res.status(201).send({ comment })
-    })
-    .catch(next)
-}
+    insertComment(review_id, username, body)
+        .then((comment) => {
+            res.status(201).send({ comment });
+        })
+        .catch(next);
+};
 
 exports.deleteComment = (req, res, next) => {
     const { comment_id } = req.params;
 
-    removeComment(comment_id).then(() => {
-        res.status(204).send();
-    })
-    .catch(next)
-}
+    removeComment(comment_id)
+        .then(() => {
+            res.status(204).send();
+        })
+        .catch(next);
+};
 
 exports.patchComment = (req, res, next) => {
     const { comment_id } = req.params;
-    const { inc_votes } = req.body;
-    updateComment(comment_id, inc_votes).then(comment => {
-        res.send({ comment })
-    })
-    .catch(next)
-}
+    const { inc_votes, body } = req.body;
+    updateComment(comment_id, inc_votes, body)
+        .then((comment) => {
+            res.send({ comment });
+        })
+        .catch(next);
+};
--- models/comments.model.js
+++ models/comments.model.js
@@ -1,10 +1,13 @@
-const db = require('../db/connection');
+const db = require("../db/connection");
 
 exports.selectComments = async (id, limit = 10, p = 1, queryKeys) => {
-    const validQueries = ['limit', 'p'];
-    if (!queryKeys.every( key => validQueries.includes(key))) {
-        return Promise.reject({status:'400', msg: 'Invalid: not a query'})
-    };
+    const validQueries = ["limit", "p"];
+    if (!queryKeys.every((key) => validQueries.includes(key))) {
+        return Promise.reject({
+            status: "400",
+            msg: "Invalid: not a query",
+        });
+    }
     const offset = (p - 1) * limit;
     const queryStr = `SELECT 
         comments.comment_id, 
@@ -15,72 +18,109 @@ exports.selectComments = async (id, limit = 10, p = 1, queryKeys) => {
         FROM comments 
         LEFT JOIN users ON comments.author = users.username 
         WHERE comments.review_id = $1
-        LIMIT $2 OFFSET $3`
-    const { rows } = await db.query(queryStr, [id, limit, offset])
-    const reviewResult = await db.query(`SELECT * FROM reviews WHERE review_id = $1`, [id])
+        LIMIT $2 OFFSET $3`;
+    const { rows } = await db.query(queryStr, [id, limit, offset]);
+    const reviewResult = await db.query(
+        `SELECT * FROM reviews WHERE review_id = $1`,
+        [id]
+    );
 
     if (reviewResult.rows.length === 0) {
         return Promise.reject({
-            status: '404',
-            msg: 'This review_id does not exist!'
+            status: "404",
+            msg: "This review_id does not exist!",
         });
-    }   else    {
+    } else {
         return rows;
     }
-}
+};
 
 exports.insertComment = async (id, name, body) => {
     const queryStr = `INSERT INTO comments 
         (body, author, review_id)
         VALUES ($1, $2, $3)
-        RETURNING *;`
+        RETURNING *;`;
 
     if (name === undefined || body === undefined) {
         return Promise.reject({
-            status: '400',
-            msg: 'Invalid post request body'
+            status: "400",
+            msg: "Invalid post request body",
         });
-    }   else if (name === null || body === null) {
+    } else if (name === null || body === null) {
         return Promise.reject({
-            status: '400',
-            msg: 'Username or post body cannot be null!'
+            status: "400",
+            msg: "Username or post body cannot be null!",
         });
-    }   else {
-        const { rows }= await db.query(queryStr, [body, name, id]);
+    } else {
+        const { rows } = await db.query(queryStr, [body, name, id]);
         return rows[0];
     }
-}
+};
 
 exports.removeComment = async (id) => {
-    const { rows } = await db
-    .query(`DELETE FROM comments WHERE comment_id = $1 RETURNING *;`, [id])
+    const { rows } = await db.query(
+        `DELETE FROM comments WHERE comment_id = $1 RETURNING *;`,
+        [id]
+    );
     if (rows.length !== 0) {
-        return rows[0]
-    }   else {
+        return rows[0];
+    } else {
         return Promise.reject({
-            status: '404',
-            msg: 'This comment_id does not exist!'
+            status: "404",
+            msg: "This comment_id does not exist!",
         });
     }
-}
+};
 
-exports.updateComment = async (id, votes) => {
-    const { rows } = await db
-        .query(`UPDATE comments SET votes = votes + $2 
-            WHERE comment_id = $1 RETURNING *`, [id, votes])
-    if (votes !== undefined) {
-        if (rows.length !== 0) {
-            return rows[0];
-        }   else {
-            return Promise.reject({ 
-                status: '404', 
-                msg: 'This comment_id does not exist!'
-            })
-        }
-    }   else {
-        return Promise.reject({ 
-            status: '400',
-            msg: 'Bad request or invalid input'
-        })
+exports.updateComment = async (id, vote, body) => {
+    let queryStr = `UPDATE comments SET `;
+    let queryValues = [id];
+    if (vote && body) {
+        queryStr += `votes = votes + $2, 
+            body = $3 `;
+        queryValues.push(vote, body);
+    } else if (vote && !body) {
+        queryStr += `votes = votes + $2 `;
+        queryValues.push(vote);
+    } else if (!vote && body) {
+        queryStr += `body = $2 `;
+        queryValues.push(body);
+    } else {
+        return Promise.reject({
+            status: "400",
+            msg: "Bad request or invalid input",
+        });
     }
-}
+    queryStr += `WHERE comment_id = $1 RETURNING *;`;
+
+    const { rows } = await db.query(queryStr, queryValues);
+    if (rows.length !== 0) {
+        return rows[0];
+    } else {
+        return Promise.reject({
+            status: "404",
+            msg: "This comment_id does not exist!",
+        });
+    }
+
+    // const { rows } = await db.query(
+    //     `UPDATE comments SET votes = votes + $2
+    //         WHERE comment_id = $1 RETURNING *`,
+    //     [id, votes]
+    // );
+    // if (votes !== undefined) {
+    //     if (rows.length !== 0) {
+    //         return rows[0];
+    //     } else {
+    //         return Promise.reject({
+    //             status: "404",
+    //             msg: "This comment_id does not exist!",
+    //         });
+    //     }
+    // } else {
+    //     return Promise.reject({
+    //         status: "400",
+    //         msg: "Bad request or invalid input",
+    //     });
+    // }
+};
