af42e2196f68ce08a768c467e92a8ffdc78a0927
cynthiaswill <yzhang4@msn.com>
2022-01-31 19:05:21 +0000

    move fetchHistory into Models

--- models/messages.model.js
+++ models/messages.model.js
@@ -2,6 +2,35 @@ const { fetchHistory } = require("../socketIO/fetchHistory");
 const { client } = require("../socketIO/socketUser");
 
 exports.fetchHistoryByRoom = async (room) => {
+  const fetchHistory = async (roomName) => {
+    const chatHistory = [];
+    try {
+      await client.connect();
+      const database = client.db("My_test_project");
+      const history = database.collection("chatHistory");
+      // query for chatHistory with the matching roomName
+      const query = { roomName: `${roomName}` };
+      const options = {
+        // sort returned documents in ascending order by title (A->Z)
+        sort: { timestamp: 1 },
+        // Include only the `username` and `text` fields in each returned document
+        projection: { _id: 1, username: 1, messageBody: 1, dateCreated: 1 },
+      };
+      const cursor = history.find(query, options);
+      // print a message if no documents were found
+      if ((await cursor.count()) === 0) {
+        console.log("No documents found!");
+      }
+      // replace console.dir with your callback to access individual elements
+      await cursor.forEach((item) => {
+        chatHistory.push(item);
+      });
+    } finally {
+      await client.close();
+    }
+    return chatHistory;
+  };
+
   return await fetchHistory(room);
 };
 
