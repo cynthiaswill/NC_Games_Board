13b8e3fdb116ca021150aa01c26cdc97b6b32ec5
cynthiaswill <yzhang4@msn.com>
2022-02-27 18:13:10 +0000

    revert socketUser

--- listen.js
+++ listen.js
@@ -1,70 +1,10 @@
-// const app = require("./app");
-// const socket = require("socket.io");
-// const {
-//   insertIntoDB,
-//   get_Last_User,
-//   user_Disconnect,
-//   join_User,
-// } = require("./socketIO/socketUser");
-
-// const { PORT = 9000 } = process.env;
-// const httpServer = app.listen(PORT, () => {
-//   console.log(`listening on port ${PORT}...`);
-// });
-
-// const io = socket(httpServer, {
-//   cors: { origin: "*" },
-// });
-
-// //listener#1: initialise socket io connection
-// io.on("connection", (socket) => {
-//   //for a new user joining the room
-//   socket.on("joinRoom", ({ username, roomName }) => {
-//     //* create user
-//     const p_user = join_User(socket.id, username, roomName);
-//     console.log(socket.id, "=id");
-//     socket.join(p_user.roomName);
-//   });
-
-//   //listener#2: user sending message
-//   socket.on("chat", (messageBody) => {
-//     //gets the room user and the message sent
-//     const p_user = get_Last_User(socket.id);
-//     console.log(p_user.username, p_user.roomName, "<<<<<<<<");
-
-//     //emit sent messages using sockitIO
-//     io.to(p_user.roomName).emit("message", {
-//       userId: p_user.id,
-//       username: p_user.username,
-//       messageBody: messageBody,
-//     });
-
-//     // insert sent message into database
-//     insertIntoDB(p_user.username, p_user.roomName, messageBody).catch(console.dir);
-//   });
-
-//   //listener#3: when the user exits the room
-//   socket.on("disconnect", async () => {
-//     //the user is deleted from array of users and a message displayed
-//     const p_user = await user_Disconnect(socket.id);
-//     console.log(p_user, "user to be disconnected");
-//     if (p_user) {
-//       io.to(p_user.roomName).emit("message", {
-//         userId: p_user.id,
-//         username: p_user.username,
-//         messageBody: `${p_user.username} has left the chat`,
-//       });
-//     }
-//   });
-// });
-
 const app = require("./app");
 const socket = require("socket.io");
 const {
+  insertIntoDB,
   get_Last_User,
   user_Disconnect,
   join_User,
-  client,
 } = require("./socketIO/socketUser");
 
 const { PORT = 9000 } = process.env;
@@ -92,42 +32,22 @@ io.on("connection", (socket) => {
     const p_user = get_Last_User(socket.id);
     console.log(p_user.username, p_user.roomName, "<<<<<<<<");
 
-    // insert sent message into database
-    async function insertIntoDB() {
-      try {
-        await client.connect();
-        const database = client.db("My_test_project");
-        const history = database.collection("chatHistory");
-        // create a document to insert
-        const doc = {
-          username: `${p_user.username}`,
-          roomName: `${p_user.roomName}`,
-          messageBody: `${messageBody}`,
-          dateCreated: new Date(),
-        };
-        const result = await history.insertOne(doc);
-        console.log(
-          `A document was inserted with the _id: ${result.insertedId} by ${p_user.username}`
-        );
-      } catch (error) {
-        console.dir(error);
-      }
-    }
-    insertIntoDB().catch(console.dir);
-
     //emit sent messages using sockitIO
     io.to(p_user.roomName).emit("message", {
       userId: p_user.id,
       username: p_user.username,
       messageBody: messageBody,
     });
+
+    // insert sent message into database
+    insertIntoDB(p_user.username, p_user.roomName, messageBody).catch(console.dir);
   });
 
   //listener#3: when the user exits the room
-  socket.on("disconnect", () => {
+  socket.on("disconnect", async () => {
     //the user is deleted from array of users and a message displayed
-    const p_user = user_Disconnect(socket.id);
-
+    const p_user = await user_Disconnect(socket.id);
+    console.log(p_user, "user to be disconnected");
     if (p_user) {
       io.to(p_user.roomName).emit("message", {
         userId: p_user.id,
--- socketIO/socketUser.js
+++ socketIO/socketUser.js
@@ -1,3 +1,127 @@
+// const { MongoClient } = require("mongodb");
+// const uri =
+//   "mongodb+srv://yzhang4:123456abc@cluster0.rspyf.mongodb.net/My_test_project?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+// const c_users = [];
+
+// async function insertIntoDB(user, room, message) {
+//   try {
+//     await client.connect();
+//     const database = client.db("My_test_project");
+//     const history = database.collection("chatHistory");
+//     // create a document to insert
+//     const doc = {
+//       username: `${user}`,
+//       roomName: `${room}`,
+//       messageBody: `${message}`,
+//       dateCreated: new Date(),
+//     };
+//     const result = await history.insertOne(doc);
+//     console.log(`A document was inserted with the _id: ${result.insertedId} by ${user}`);
+//   } finally {
+//     await client.close();
+//   }
+// }
+
+// const getUsersList = async () => {
+//   try {
+//     await client.connect();
+//     const database = client.db("My_test_project");
+//     const history = database.collection("chatHistory");
+//     const query = { title: "online users list" };
+//     const options = {
+//       projection: { onlineUsers: 1 },
+//     };
+//     const list = await history.findOne(query, options);
+//     console.log(list.onlineUsers, "online_user_list from db");
+//     return list.onlineUsers;
+//   } catch (error) {
+//     console.dir(error);
+//   }
+// };
+
+// const updateUsersList = async (onlineUsers) => {
+//   try {
+//     await client.connect();
+
+//     const database = client.db("My_test_project");
+//     const history = database.collection("chatHistory");
+
+//     // create a filter for a movie to update
+//     const filter = { title: "online users list" };
+
+//     // this option instructs the method to create a document if no documents match the filter
+//     const options = { upsert: true };
+
+//     // create a document that sets the plot of the movie
+//     const updateDoc = {
+//       $set: {
+//         onlineUsers: [...onlineUsers],
+//       },
+//     };
+
+//     const result = await history.updateOne(filter, updateDoc, options);
+//     console.log(
+//       `${result.matchedCount} document(s) matched the filter, updated ${result.modifiedCount} document(s)`
+//     );
+//   } catch (error) {
+//     console.dir(error);
+//   }
+// };
+
+// // joins the user to the specific chatroom
+// async function join_User(id, username, roomName) {
+//   const p_user = { id, username, roomName };
+//   const onlineUsers = await getUsersList();
+//   console.log(onlineUsers, "online_users before update list");
+//   c_users.push(p_user);
+
+//   !onlineUsers.includes(username) && onlineUsers.push(username);
+
+//   console.log(onlineUsers, "online_users after update list");
+
+//   updateUsersList(onlineUsers);
+
+//   return p_user;
+// }
+
+// console.log("current_users", c_users);
+
+// function get_Last_User(id) {
+//   const matchedUsers = c_users.filter((user) => {
+//     return user.id === id;
+//   });
+//   const users = [...matchedUsers];
+//   return users[users.length - 1];
+// }
+
+// // called when the user leaves the chat and its user object deleted from array
+// async function user_Disconnect(id) {
+//   const onlineUsers = await getUsersList();
+//   const userToDelete = get_Last_User(id);
+//   console.log(onlineUsers, "online_users before delete from list");
+
+//   if (userToDelete) {
+//     const index = onlineUsers.findIndex((username) => username === userToDelete.username);
+//     onlineUsers.splice(index, 1);
+//     updateUsersList(onlineUsers);
+//     console.log(onlineUsers, "online_users after someone disconnects");
+//     return userToDelete;
+//   }
+// }
+
+// module.exports = {
+//   join_User,
+//   insertIntoDB,
+//   get_Last_User,
+//   user_Disconnect,
+//   client,
+// };
+
 const { MongoClient } = require("mongodb");
 const uri =
   "mongodb+srv://yzhang4:123456abc@cluster0.rspyf.mongodb.net/My_test_project?retryWrites=true&w=majority";
@@ -7,6 +131,7 @@ const client = new MongoClient(uri, {
 });
 
 const c_users = [];
+const onlineUsers = [];
 
 async function insertIntoDB(user, room, message) {
   try {
@@ -27,24 +152,7 @@ async function insertIntoDB(user, room, message) {
   }
 }
 
-const getUsersList = async () => {
-  try {
-    await client.connect();
-    const database = client.db("My_test_project");
-    const history = database.collection("chatHistory");
-    const query = { title: "online users list" };
-    const options = {
-      projection: { onlineUsers: 1 },
-    };
-    const list = await history.findOne(query, options);
-    console.log(list.onlineUsers, "online_user_list from db");
-    return list.onlineUsers;
-  } catch (error) {
-    console.dir(error);
-  }
-};
-
-const updateUsersList = async (onlineUsers) => {
+const updateUsersList = async () => {
   try {
     await client.connect();
 
@@ -74,17 +182,19 @@ const updateUsersList = async (onlineUsers) => {
 };
 
 // joins the user to the specific chatroom
-async function join_User(id, username, roomName) {
+function join_User(id, username, roomName) {
   const p_user = { id, username, roomName };
-  const onlineUsers = await getUsersList();
-  console.log(onlineUsers, "online_users before update list");
+
   c_users.push(p_user);
 
-  !onlineUsers.includes(username) && onlineUsers.push(username);
+  c_users.forEach((user) => {
+    if (!onlineUsers.includes(user.username)) {
+      onlineUsers.push(user.username);
+    }
+  });
+  console.log(onlineUsers, "online_users");
 
-  console.log(onlineUsers, "online_users after update list");
-
-  updateUsersList(onlineUsers);
+  updateUsersList();
 
   return p_user;
 }
@@ -100,24 +210,23 @@ function get_Last_User(id) {
 }
 
 // called when the user leaves the chat and its user object deleted from array
-async function user_Disconnect(id) {
-  const onlineUsers = await getUsersList();
-  const userToDelete = get_Last_User(id);
-  console.log(onlineUsers, "online_users before delete from list");
-
-  if (userToDelete) {
-    const index = onlineUsers.findIndex((username) => username === userToDelete.username);
-    onlineUsers.splice(index, 1);
-    updateUsersList(onlineUsers);
-    console.log(onlineUsers, "online_users after someone disconnects");
-    return userToDelete;
+function user_Disconnect(id) {
+  const index = c_users.findIndex((p_user) => p_user.id === id);
+  const index2 = onlineUsers.findIndex(
+    (username) => username === c_users[index].username
+  );
+  if (index !== -1) {
+    onlineUsers.splice(index2, 1);
+    updateUsersList();
+    console.log(index2, onlineUsers, "<<online_users");
+    return c_users.splice(index, 1)[0];
   }
 }
 
 module.exports = {
   join_User,
-  insertIntoDB,
   get_Last_User,
   user_Disconnect,
+  insertIntoDB,
   client,
 };
