f74b4fc468182381ea2601f33737fa8e686b2084
cynthiaswill <yzhang4@msn.com>
2022-03-05 14:11:35 +0000

    hides mc password in dotenv

--- socketIO/socketUser.js
+++ socketIO/socketUser.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const { MongoClient } = require("mongodb");
-const uri =
-  "mongodb+srv://yzhang4:123456abc@cluster0.rspyf.mongodb.net/My_test_project?retryWrites=true&w=majority";
+const uri = `mongodb+srv://yzhang4:${process.env.MCPASSWORD}@cluster0.rspyf.mongodb.net/My_test_project?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
